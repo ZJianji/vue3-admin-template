0468e3b085da12643a06b9334cd787e130063a80
ZJianJi <1774031367@qq.com>
2024-10-10 19:58:14 +0800

    feat:初始化

--- src/views/menu/menu-1-1.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="menu-1">
-    <el-alert :closable="false" title="菜单 2-1" type="warning" effect="dark" />
-  </div>
-</template>
--- src/views/menu/menu-2.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="menu-2">
-    <el-alert :closable="false" title="菜单 2-2" type="info" effect="dark" />
-  </div>
-</template>
